8f7fb12e9c71e60983e41a0daf4b92c275de458d
Gerard Rius <gerardriusriu@gmail.com>
2023-03-16 15:52:34 +0100

    Fixed trucks and included all trips in data

--- pages/01_📂_1.Data.py
+++ pages/01_📂_1.Data.py
@@ -6,21 +6,19 @@ import src.streamlit_cleaning as st_cleaning
 st.set_page_config(page_title="Data Sources", page_icon="📂")
 
 # 1. Show the data
-st.write('''
-# Project DataFrames.
-Below you can dive deep into the resulting dataframes obtained after cleaning and enriching the initial files.
-''')
+st.header('Project DataFrames')
 
-dataframe = st.selectbox('Choose one topic', ['Bike trips', 'Truck trips'])
+st.markdown('Below you can dive deeper into the some of the resulting dataframes obtained after cleaning and enriching the initial files. Bike trips dataframe is mainly the transformed initial _csv_ file after some cleaning, whereas truck and all trips represent some of the resulting dataframes obtained after tracking _non-human_ trips.')
+
+dataframe = st.selectbox('Choose one topic', ['Bike trips', 'Truck trips', 'All trips'])
 n = st.selectbox('Choose the number of rows to be displayed', [5, 10, 20, 50])
 
 if dataframe == 'Bike trips':
     df = pd.read_csv('data/april_2014.csv')
 elif dataframe == 'Truck trips':
-    df = pd.read_csv('data/truck_transfers.csv', index_col='Unnamed: 0')
+    df = pd.read_csv('data/truck_transfers.csv')
+elif dataframe == 'All trips':
+    df = pd.read_csv('data/all_trips.csv')
 
 
 st.dataframe(st_cleaning.show_df_sample(df, n))
-
-
-st.write('Source of file can be found at https://citibikenyc.com/system-data')
